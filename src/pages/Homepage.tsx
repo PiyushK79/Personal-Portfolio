8dfa8ce3e43394b77fcc1c7143e0a24acfaad10c
Piyush <piyushkakde79@gmail.com>
2024-03-05 23:10:48 +0530

    updated

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -61,7 +61,7 @@ const Homepage1 = () => {
             direction="left"
             gradient
             gradientColor=""
-            speed={150}
+            speed={100}
             gradientWidth={30}
             pauseOnHover
           >
